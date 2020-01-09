a622eb6204abfe568f08513f95bd9310869a3d1b
farooq azam khan <farooq1.khan@ryerson.ca>
2020-01-09 18:41:01 -0500

    added foerign field to player and team serializers

--- player/api/serializers.py
+++ player/api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from player.models import Player
+from team.models import Team
 
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    team = serializers.SlugRelatedField(read_only=True, slug_field='name')
+
     class Meta:
         model = Player
         fields = ['first_name',
                   'last_name',
-                  'number']
+                  'number',
+                  'team']
--- team/api/serializers.py
+++ team/api/serializers.py
@@ -4,6 +4,9 @@ from team.models import Team
 
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
+    league = serializers.SlugRelatedField(read_only=True, slug_field='name')
+
     class Meta:
         model = Team
-        fields = ['name', 'location', 'arena_name', 'wins', 'ties', 'losses']
+        fields = ['name', 'location', 'arena_name',
+                  'wins', 'ties', 'losses', 'league']
