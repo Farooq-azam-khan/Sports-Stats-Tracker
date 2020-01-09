f5634368ff95bc7ad7302a59ab617b60db4dc6b2
farooq azam khan <farooq1.khan@ryerson.ca>
2020-01-09 17:53:24 -0500

    made landing page

--- sport_tracker/urls.py
+++ sport_tracker/urls.py
@@ -29,11 +29,11 @@ from django.views.generic import TemplateView
 # from .api import
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     # path('api/', include('sport_tracker.api')),
     path('api/leagues/', include('sport.api.urls'), name='leauge-list'),
-    path('api/teams/', include('team.api.urls')),
-    path('api/players/', include('player.api.urls')),
+    path('api/teams/', include('team.api.urls'), name='team-list'),
+    path('api/players/', include('player.api.urls'), name='players-list'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
